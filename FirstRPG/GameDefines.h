1b020f725cc14c7bbc845c2776d7a65cfc0e25c9
przempore <przempore@gmail.com>
2014-09-21 01:55:26 +0200

    test

--- FirstRPG/GameDefines.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "stdafx.h"
-
-static const int WINDOW_WIDTH = 800;
-static const int WINDOW_HIGHT = 600;
-static const int CHARACTER_MOVE_SPEED = 5;
--- FirstRPG/stdafx.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
